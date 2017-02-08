10d3e4161c7e112aa8d675be6bb4eb88e8ba14d6
Taly <talyrus@gmail.com>
2017-02-08 10:30:35 +0300

    Решения задач курса JavaRush 2.0

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task01.task0138;
+
+/* 
+Любимое стихотворение
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        //напишите тут ваш код
+        System.out.println("Наша Таня громко плачет");
+        System.out.println("Уронила Таня мячик.");
+        System.out.println("Тише, Танечка, не плач,");
+        System.out.println("Не утонет в речке мяч!");
+
+    }
+}
