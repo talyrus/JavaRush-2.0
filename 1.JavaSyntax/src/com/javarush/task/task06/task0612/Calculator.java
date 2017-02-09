36372372bee570851c2ca0a2c8df0dded6128e98
Taly <talyrus@gmail.com>
2017-02-09 09:45:50 +0300

    Решения задач курса JavaRush 2.0

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0612/Calculator.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task06.task0612;
+
+/* 
+Калькулятор
+*/
+
+public class Calculator {
+    public static int plus(int a, int b) {
+        //напишите тут ваш код
+        return a + b;
+    }
+
+    public static int minus(int a, int b) {
+        //напишите тут ваш код
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        //напишите тут ваш код
+        return a * b;
+    }
+
+    public static double division(int a, int b) {
+        //напишите тут ваш код
+        return (double) a / b;
+    }
+
+    public static double percent(int a, int b) {
+        //напишите тут ваш код
+        return (double) a * b / 100;
+    }
+
+    public static void main(String[] args) {
+        /*System.out.println(plus(100, 10));
+        System.out.println(minus(100, 10));
+        System.out.println(multiply(100, 10));
+        System.out.println(division(100, 10));
+        System.out.println(percent(100, 10));*/
+    }
+}
