ef081c1439293b9cec79e02ec441c7bed3cdb373
Taly <talyrus@gmail.com>
2017-02-14 16:45:44 +0300

    Решения задач курса JavaRush 2.0

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1409/Bridge.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task14.task1409;
+
+/**
+ * Created by Taly on 14.02.2017.
+ */
+public interface Bridge {
+    int getCarsCount();
+}
---
+++ 2.JavaCore/src/com/javarush/task/task14/task1409/SuspensionBridge.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task14.task1409;
+
+/**
+ * Created by Taly on 14.02.2017.
+ */
+public class SuspensionBridge implements Bridge {
+    @Override
+    public int getCarsCount() {
+        return 5;
+    }
+}
