afaba192099f2241e6222dcab9b50ce37037b858
Taly <talyrus@gmail.com>
2017-02-14 17:29:51 +0300

    Решения задач курса JavaRush 2.0

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1410/BubblyWine.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task14.task1410;
+
+/**
+ * Created by Taly on 14.02.2017.
+ */
+public class BubblyWine extends Wine {
+    public String getHolidayName(){
+        return "Новый Год";
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task14/task1410/Wine.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task14.task1410;
+
+/**
+ * Created by Taly on 14.02.2017.
+ */
+public class Wine extends Drink {
+    public String getHolidayName(){
+        return "День Рождения";
+    }
+}
