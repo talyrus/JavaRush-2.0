90a728ce8bf733213638094e25c8ed43b788dec3
Taly <talyrus@gmail.com>
2017-03-05 19:55:12 +0300

    Решения задач курса JavaRush 2.0

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1502/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task15.task1502;
+
+/* 
+ООП - Наследование животных
+*/
+
+public class Solution {
+    //добавьте классы Goose и Dragon тут
+    public static class Goose extends SmallAnimal {
+        @Override
+        public String getSize() {
+            return "Гусь маленький, " + super.getSize();
+        }
+    }
+    public static class Dragon extends BigAnimal{
+        public String getSize() {
+            return "Дракон большой, " + super.getSize();
+        }
+    }
+    public static void main(String[] args) {
+        /*Goose gg = new Goose();
+        System.out.println(gg.getSize());
+        Dragon dd = new Dragon();
+        System.out.println(dd.getSize());*/
+    }
+
+    public static class BigAnimal {
+        protected String getSize() {
+            return "как динозавр";
+        }
+    }
+
+    public static class SmallAnimal {
+        String getSize() {
+            return "как кошка";
+        }
+    }
+
+
+}
