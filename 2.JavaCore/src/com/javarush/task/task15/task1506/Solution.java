739ee116b51379c8306869a12c1b5fc27f6bb3b4
Taly <talyrus@gmail.com>
2017-03-07 15:28:55 +0300

    Решения задач курса JavaRush 2.0

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1506/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task15.task1506;
+
+/* 
+Что-то лишнее
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print((double) 1);
+        print((Object) 1);
+        print((double) 1);
+        print((Integer) 1);
+        print((double) 1);
+    }
+
+    public static void print(Integer i) {
+        System.out.println("Это Integer");
+    }
+
+    /*public static void print(int i) {
+        System.out.println("Это Integer");
+    }
+*/
+    /*public static void print(Short i) {
+        System.out.println("Это Object");
+    }*/
+
+    public static void print(Object i) {
+        System.out.println("Это Object");
+    }
+
+    public static void print(double i) {
+        System.out.println("Это double");
+    }
+
+   /* public static void print(Double i) {
+        System.out.println("Это double");
+    }*/
+
+    /*public static void print(float i) {
+        System.out.println("Это Double");
+    }*/
+}
