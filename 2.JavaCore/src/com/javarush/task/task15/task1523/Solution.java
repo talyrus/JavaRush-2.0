db6f4dbe5e263a1994ff054a1e97ef4ef9947c14
Taly <talyrus@gmail.com>
2017-03-10 08:25:32 +0300

    Решения задач курса JavaRush 2.0

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public Solution(String a){}
+
+    protected Solution(Integer b){}
+
+    Solution(Object o){}
+
+    private Solution(char c){}
+
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task15.task1523;
+
+
+/**
+ * Created by Taly on 09.03.2017.
+ */
+public class SubSolution extends Solution {
+    public SubSolution(String a) {
+        super(a);
+    }
+
+    SubSolution(Integer b) {
+        super(b);
+    }
+
+    protected SubSolution(Object o) {
+        super(o);
+    }
+}
