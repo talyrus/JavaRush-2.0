4e602b69d5c8d553df757254aebcfc1d865ce47c
Taly <talyrus@gmail.com>
2017-03-11 22:55:47 +0300

    Решения задач курса JavaRush 2.0

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1529/Helicopter.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task15.task1529;
+
+/**
+ * Created by Taly on 11.03.2017.
+ */
+public class Helicopter implements Flyable {
+
+    @Override
+    public void fly() {
+
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1529/Plane.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task15.task1529;
+
+/**
+ * Created by Taly on 11.03.2017.
+ */
+public class Plane implements Flyable {
+    public Plane(int pass) {
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
