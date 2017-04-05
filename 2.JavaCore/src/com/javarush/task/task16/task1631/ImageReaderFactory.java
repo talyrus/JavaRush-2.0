b1d107fa105d3d30817e06d8e515122ef01e42e3
Taly <talyrus@gmail.com>
2017-04-05 16:19:14 +0300

    Решения задач курса JavaRush 2.0

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1631/ImageReaderFactory.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task16.task1631;
+
+import com.javarush.task.task16.task1631.common.*;
+
+import java.io.Reader;
+
+/**
+ * Created by Taly on 05.04.2017.
+ */
+public class ImageReaderFactory {
+	public static ImageReader getImageReader (ImageTypes imageTypes) throws IllegalArgumentException {
+		if (imageTypes == ImageTypes.BMP) {
+			return new BmpReader();
+		} else if (imageTypes == ImageTypes.JPG) {
+			return new JpgReader();
+		} else if (imageTypes == ImageTypes.PNG) {
+			return new PngReader();
+		} else throw new IllegalArgumentException("Неизвестный тип картинки") ;
+	}
+}
