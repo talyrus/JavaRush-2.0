52a62cb70ea9db4a3da0830a395de563ad2bb602
Taly <talyrus@gmail.com>
2017-04-05 15:52:12 +0300

    Решения задач курса JavaRush 2.0

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1630/Solution.java
@@ -0,0 +1,78 @@
+package com.javarush.task.task16.task1630;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static String firstFileName;
+    public static String secondFileName;
+
+    //add your code here - добавьте код тут
+    static {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            firstFileName = reader.readLine();
+            secondFileName = reader.readLine();
+            reader.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        systemOutPrintln(firstFileName);
+        systemOutPrintln(secondFileName);
+    }
+
+    public static void systemOutPrintln(String fileName) throws InterruptedException {
+        ReadFileInterface f = new ReadFileThread();
+        f.setFileName(fileName);
+        f.start();
+        //add your code here - добавьте код тут
+        f.join();
+        System.out.println(f.getFileContent());
+    }
+
+    public interface ReadFileInterface {
+
+        void setFileName(String fullFileName);
+
+        String getFileContent();
+
+        void join() throws InterruptedException;
+
+        void start();
+    }
+
+    //add your code here - добавьте код тут
+    public static class ReadFileThread extends Thread implements ReadFileInterface {
+        private String fileName, fileContent = "";
+        @Override
+        public void setFileName(String fullFileName) {
+            fileName = fullFileName;
+        }
+
+        @Override
+        public String getFileContent() {
+            return fileContent;
+        }
+
+        @Override
+        public void run() {
+            String text;
+            try {
+                FileReader fileReader = new FileReader(fileName);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+                while (( text = bufferedReader.readLine()) != null) {
+                    fileContent = fileContent + " " + text;
+                }
+                bufferedReader.close();
+            } catch (IOException ex)
+            {
+
+            }
+        }
+    }
+}
