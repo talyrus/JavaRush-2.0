51146102f28ee8f0f383d85670c218d8e9aec82d
Taly <talyrus@gmail.com>
2017-06-21 12:04:04 +0300

    Решения задач курса JavaRush 2.0 (сериализация класса)

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2009/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task20.task2009;
+
+import java.io.*;
+
+/*
+Как сериализовать static?
+*/
+public class Solution {
+    public static class ClassWithStatic implements Serializable {
+        public static String staticString = "it's test static string";
+        public int i;
+        public int j;
+
+        public static void serializeStatic(ObjectOutputStream oos) throws IOException {
+            oos.writeObject(staticString);
+        }
+        public static void deserializeStatic(ObjectInputStream ois) throws IOException {
+            staticString = String.valueOf(ois.read());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        ClassWithStatic cws = new ClassWithStatic();
+        String path = "d:\\!Virus\\test.txt";
+        cws.i = 111;
+        cws.j = 222;
+
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path));
+        ClassWithStatic.serializeStatic(oos);
+        oos.writeObject(cws);
+        oos.close();
+
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path));
+        ClassWithStatic.deserializeStatic(ois);
+        ClassWithStatic cws2 = (ClassWithStatic)ois.readObject();
+        ois.close();
+
+        System.out.println(cws2.i + " " + cws2.j + " " + cws2.toString());
+    }
+}
