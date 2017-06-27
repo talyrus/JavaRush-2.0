864240e6c42c31273ff1071243efeacc3ef8a58a
Taly <talyrus@gmail.com>
2017-06-27 14:09:42 +0300

    Решения задач курса JavaRush 2.0 (Упрощение)

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2103/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2103/Solution.java
@@ -5,7 +5,7 @@ package com.javarush.task.task21.task2103;
 */
 public class Solution {
     public static boolean calculate(boolean a, boolean b, boolean c, boolean d) {
-        //return (a && b && c && !d) || (!a && c) || (!b && c) || (c && d);
+        //return (a && b && c && !d) || (!a && c) || (!b && c) || (c && d); - строку при сдаче удалить
        return c;
     }
 
