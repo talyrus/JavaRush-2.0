7b644032750b3e908893f5aa1e0286c9d4972950
Taly <talyrus@gmail.com>
2017-06-27 14:07:47 +0300

    Решения задач курса JavaRush 2.0 (Проверка значений модификаторов)

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2103/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2103/Solution.java
@@ -5,8 +5,8 @@ package com.javarush.task.task21.task2103;
 */
 public class Solution {
     public static boolean calculate(boolean a, boolean b, boolean c, boolean d) {
-        return c;
-
+        //return (a && b && c && !d) || (!a && c) || (!b && c) || (c && d);
+       return c;
     }
 
     public static void main(String[] args) {
