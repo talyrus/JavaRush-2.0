0d66789c6fb46e3b0c2ebfd4507642e2cadd4e0e
Taly <talyrus@gmail.com>
2017-07-05 15:02:15 +0300

    Решения задач курса JavaRush 2.0 (Клонирование)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2109/Solution.java
@@ -0,0 +1,62 @@
+package com.javarush.task.task21.task2109;
+
+/* 
+Запретить клонирование
+*/
+public class Solution {
+    public static class A implements Cloneable {
+        private int i;
+        private int j;
+
+        public A(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+
+        public int getI() {
+            return i;
+        }
+
+        public int getJ() {
+            return j;
+        }
+    }
+
+    public static class B extends A {
+        private String name;
+
+        public B(int i, int j, String name) {
+            super(i, j);
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            throw new CloneNotSupportedException();
+        }
+    }
+
+    public static class C extends B implements Cloneable {
+        public C(int i, int j, String name) {
+            super(i, j, name);
+        }
+
+        @Override
+        protected C clone() throws CloneNotSupportedException {
+            C newC;
+            int i = getI();
+            int j = getJ();
+            String name = getName();
+            newC = new C(i, j, name);
+            return newC;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
