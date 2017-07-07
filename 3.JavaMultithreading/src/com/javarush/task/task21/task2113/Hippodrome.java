2eb673697d846d29d971358c9b49033153b39a2c
Taly <talyrus@gmail.com>
2017-07-07 10:34:10 +0300

    Решения задач курса JavaRush 2.0 (Game Hippodrome)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task21.task2113;
+
+/**
+ * Created by Taly on 07.07.2017.
+ */
+public class Hippodrome {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task21.task2113;
+
+/**
+ * Created by Taly on 07.07.2017.
+ */
+public class Horse {
+}
