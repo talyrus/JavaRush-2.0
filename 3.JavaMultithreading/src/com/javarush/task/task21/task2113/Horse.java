280268f873d9ef6d2cd80dd74f9f62fe24964d66
Taly <talyrus@gmail.com>
2017-07-07 11:38:33 +0300

    Решения задач курса JavaRush 2.0 (Game Hippodrome)

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
@@ -6,4 +6,34 @@ public class Horse { //1. Создадим класс
 	String name;    //4. создадим необходимые поля
 	double speed;
 	double distance;
+
+	public Horse(String name, double speed, double distance) { //5. Создадим конструктор класса Horse
+		this.name = name;
+		this.speed = speed;
+		this.distance = distance;
+	}
+
+	public String getName() { //5. Создадим getter’ы и setter’ы для всех полей класса Horse.
+		return name;
+	}
+
+	public double getSpeed() {
+		return speed;
+	}
+
+	public double getDistance() {
+		return distance;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
 }
