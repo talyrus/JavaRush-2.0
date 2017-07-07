ec3f8da7ca2c79d27de57b3f47499f61261030c6
Taly <talyrus@gmail.com>
2017-07-07 11:25:05 +0300

    Решения задач курса JavaRush 2.0 (Game Hippodrome)

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,8 +1,17 @@
 package com.javarush.task.task21.task2113;
+
+import java.util.List;
+
 /**
  * Created by Taly on 07.07.2017.
  */
 public class Hippodrome {
+	private List<Horse> horses; //создали поле типа List<Horse>
+
+	public List<Horse> getHorses() { // создали метод, возвращающий список
+		return horses;
+	}
+
 	public static void main(String[] args) {
 
 	}
