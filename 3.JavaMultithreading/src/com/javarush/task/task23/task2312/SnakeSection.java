1e87db4b7e3a04b4315453177cf36cd69f0b4349
Taly <talyrus@gmail.com>
2017-07-14 15:52:58 +0300

    Решения задач курса JavaRush 2.0 (Игра "Змейка")

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
@@ -4,4 +4,18 @@ package com.javarush.task.task23.task2312;
  * Created by Taly on 14.07.2017.
  */
 public class SnakeSection {
+	private int x, y;
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public SnakeSection(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 }
