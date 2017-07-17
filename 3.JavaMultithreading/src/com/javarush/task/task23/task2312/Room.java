60be5592c26c9357eecf24c7f5b7d6cceccf4b75
Taly <talyrus@gmail.com>
2017-07-17 14:54:08 +0300

    Решения задач курса JavaRush 2.0 (Игра "Змейка")

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
@@ -4,6 +4,48 @@ package com.javarush.task.task23.task2312;
  * Created by Taly on 14.07.2017.
  */
 public class Room {
+	private int width, height;
+	private Snake snake;
+	private Mouse mouse;
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public Snake getSnake() {
+		return snake;
+	}
+
+	public void setSnake(Snake snake) {
+		this.snake = snake;
+	}
+
+	public Mouse getMouse() {
+		return mouse;
+	}
+
+	public void setMouse(Mouse mouse) {
+		this.mouse = mouse;
+	}
+
+	public Room(int width, int height, Snake snake) {
+		this.width = width;
+		this.height = height;
+		this.snake = snake;
+	}
+
 	public static void main(String[] args) {
 
 	}
