0b68804c5178eb06f926a85697446adb7764991e
Taly <talyrus@gmail.com>
2017-07-17 14:39:48 +0300

    Решения задач курса JavaRush 2.0 (Игра "Змейка")

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task23.task2312;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,4 +26,29 @@ public class Snake {
 	public void setDirection(SnakeDirection direction) {
 		this.direction = direction;
 	}
+
+	public Snake(int x, int y) {
+		this.sections = new ArrayList<>();
+		SnakeSection snakeHead = new SnakeSection(x, y);
+		sections.add(snakeHead);
+		this.isAlive = true;
+	}
+
+	public int getX() {
+		return sections.get(0).getX();
+	}
+
+	public int getY() {
+		return sections.get(0).getY();
+	}
+
+	public void move() {
+
+	}
+
+
+
+
 }
+
+
