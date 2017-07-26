ef9d8867a285e86f48fd9d8b7d00839a6bfa7242
Taly <talyrus@gmail.com>
2017-07-26 16:11:19 +0300

    Решения задач курса JavaRush 2.0 (Game Arkanoid)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -4,6 +4,24 @@ package com.javarush.task.task24.task2413;
  * Created by Taly on 26.07.2017.
  */
 public class Arkanoid {
+	private int width, height;
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
 	public static void main(String[] args) {
 
 	}
