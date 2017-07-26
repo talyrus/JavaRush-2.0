db3740b0011b97c94b75de13edc74afcb57d0a60
Taly <talyrus@gmail.com>
2017-07-26 16:09:09 +0300

    Решения задач курса JavaRush 2.0 (Game Arkanoid)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task24.task2413;
+
+/**
+ * Created by Taly on 26.07.2017.
+ */
+public class Arkanoid {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task24.task2413;
+
+/**
+ * Created by Taly on 26.07.2017.
+ */
+public class Ball {
+}
