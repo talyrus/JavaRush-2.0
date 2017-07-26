3bdd891ef3b3e9df6c9cc367379e088a56fd8132
Taly <talyrus@gmail.com>
2017-07-26 16:45:56 +0300

    Решения задач курса JavaRush 2.0 (Game Arkanoid)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -4,4 +4,7 @@ package com.javarush.task.task24.task2413;
  * Created by Taly on 26.07.2017.
  */
 public class Ball extends BaseObject {
+	public Ball(double x, double y, double radius) {
+		super(x, y, radius);
+	}
 }
--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -4,4 +4,7 @@ package com.javarush.task.task24.task2413;
  * Created by Taly on 26.07.2017.
  */
 public class Stand extends BaseObject {
+	public Stand(double x, double y, double radius) {
+		super(x, y, radius);
+	}
 }
