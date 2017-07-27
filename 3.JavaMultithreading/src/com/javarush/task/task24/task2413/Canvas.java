416add2a8234ff765a1ca772a85a11c4a53b6340
Taly <talyrus@gmail.com>
2017-07-27 13:41:38 +0300

    Решения задач курса JavaRush 2.0 (Game Arkanoid)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -4,4 +4,36 @@ package com.javarush.task.task24.task2413;
  * Created by Taly on 26.07.2017.
  */
 public class Canvas {
+	private int width, height;
+	private char[][] matrix;
+
+	public Canvas(int width, int height) {
+		this.width = width;
+		this.height = height;
+		matrix = new char[height + 2][width + 2];
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public char[][] getMatrix() {
+		return matrix;
+	}
+
+	public void setMatrix(char[][] matrix) {
+		this.matrix = matrix;
+	}
 }
