65ddcf55ae25fa291c4479316134b1a58f23a4f4
Taly <talyrus@gmail.com>
2017-07-28 11:53:45 +0300

    Решения задач курса JavaRush 2.0 (Game Arkanoid)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -4,6 +4,25 @@ package com.javarush.task.task24.task2413;
  * Created by Taly on 26.07.2017.
  */
 public class Stand extends BaseObject {
+	private double speed, direction;
+	//direction -(направление движения по оси x:
+	// 1 — вправо, -1 — влево, 0 — начальное значение, стоим на месте)
+
+
+	public double getSpeed() {
+		return speed;
+	}
+
+	public double getDirection() {
+		return direction;
+	}
+
+	public Stand(double x, double y) {
+		super(x, y, 3);
+		speed = 1;
+		direction = 0;
+	}
+
 	public Stand(double x, double y, double radius) {
 		super(x, y, radius);
 	}
