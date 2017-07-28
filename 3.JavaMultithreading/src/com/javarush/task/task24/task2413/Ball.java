12609ca13314d592ff36686432b61ab8a8341251
Taly <talyrus@gmail.com>
2017-07-28 08:54:45 +0300

    Решения задач курса JavaRush 2.0 (Game Arkanoid)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -4,8 +4,33 @@ package com.javarush.task.task24.task2413;
  * Created by Taly on 26.07.2017.
  */
 public class Ball extends BaseObject {
+	private double speed, direction, dx, dy;
+	private boolean isFrozen = true;
+
+	public double getSpeed() {
+		return speed;
+	}
+
+	public double getDirection() {
+		return direction;
+	}
+
+	public double getDx() {
+		return dx;
+	}
+
+	public double getDy() {
+		return dy;
+	}
+
 	public Ball(double x, double y, double radius) {
 		super(x, y, radius);
+	}
+
+	public Ball(double x, double y, double speed, double direction) {
+		super(x, y, 1);
+		this.speed = speed;
+		this.direction = direction;
 	}
 
 	@Override
