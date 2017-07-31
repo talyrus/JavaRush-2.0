6349153d4f259e0a199b09e8c5e6a27c7e507c8a
Taly <talyrus@gmail.com>
2017-07-31 15:32:03 +0300

    Решения задач курса JavaRush 2.0 (Интерфейсы-маркеры)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task24.task2401;
+
+/**
+ * Created by Taly on 31.07.2017.
+ */
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+	public void method1() {
+		System.out.println("1");
+	}
+
+	public void method2() {
+		System.out.println("2");
+	}
+
+}
