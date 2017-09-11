64765fe15f23c15d392bf19ad665207a9619029f
Taly <talyrus@gmail.com>
2017-09-11 17:12:00 +0300

    Решения задач курса JavaRush 2.0 (Абстрактный класс от интерфейса)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2409/Item.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task24.task2409;
+
+/**
+ * Created by Taly on 11.09.2017.
+ */
+public interface Item {
+	int getId();
+
+	double getPrice();
+
+	String getTM();
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2409/Jeans.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task24.task2409;
+
+/**
+ * Created by Taly on 11.09.2017.
+ */
+public interface Jeans extends Item {
+	int getLength();
+
+	int getSize();
+}
