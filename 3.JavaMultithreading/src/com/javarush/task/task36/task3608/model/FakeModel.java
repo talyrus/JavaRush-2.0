c3c4cd378b9c77a7d36351c9504756987badf592
Taly <talyrus@gmail.com>
2017-09-20 14:49:33 +0300

    Решения задач курса JavaRush 2.0 (MVC1)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/FakeModel.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+
+import java.util.Arrays;
+
+/**
+ * Created by Taly on 20.09.2017.
+ */
+public class FakeModel implements Model {
+	private ModelData modelData = new ModelData();
+
+	@Override
+	public void loadUsers() {
+		modelData.setUsers(Arrays.asList(new User("A", 1, 1),
+				new User("B", 2, 1)));
+	}
+
+	@Override
+	public ModelData getModelData() {
+		return this.modelData;
+	}
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/Model.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task36.task3608.model;
+
+/**
+ * Created by Taly on 20.09.2017.
+ */
+public interface Model {
+	ModelData getModelData();
+
+	void loadUsers();
+}
