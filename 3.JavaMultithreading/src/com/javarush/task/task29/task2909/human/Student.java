f46400612d3aa9b94c10d3881ea8169a7dcc4418
Taly <talyrus@gmail.com>
2017-11-17 14:37:57 +0300

    Решения задач курса JavaRush 2.0 (Big Task - Refactoring 4)

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
@@ -2,10 +2,10 @@ package com.javarush.task.task29.task2909.human;
 
 import java.util.Date;
 
-public class Student extends Human {
+public class Student extends UniversityPerson {
 
 	private double averageGrade;
-	private String university;
+
 	private Date beginningOfSession;
 	private Date endOfSession;
 	private int course;
@@ -26,13 +26,7 @@ public class Student extends Human {
 	public void learn() {
 	}
 
-	public String getUniversity() {
-		return university;
-	}
 
-	public void setUniversity(String university) {
-		this.university = university;
-	}
 
 	public void printData() {
 		System.out.println("Студент: " + name);
