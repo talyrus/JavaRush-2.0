6d5311130d24e284256aeaa20d95cc272188f9ab
Taly <talyrus@gmail.com>
2017-11-20 10:48:02 +0300

    Решения задач курса JavaRush 2.0 (Big Task - Refactoring 9)

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
@@ -9,14 +9,13 @@ public class Human implements Alive {
 	private int id;
 	protected int age;
 	protected String name;
+	protected Size size;
+	private BloodGroup bloodGroup;
 
-	protected int[] size;
-
-	public static final int FIRST = 1;
-	public static final int SECOND = 2;
-	public static final int THIRD = 3;
-	public static final int FOURTH = 4;
-	private int bloodGroup;
+	public class Size {
+		public int height;
+		public int weight;
+	}
 
 	private List<Human> children = new ArrayList<>();
 
@@ -32,11 +31,12 @@ public class Human implements Alive {
 		children.remove(human);
 	}
 
-	public void setBloodGroup(int code) {
-		bloodGroup = code;
+
+	public void setBloodGroup(BloodGroup bloodGroup) {
+		this.bloodGroup = bloodGroup;
 	}
 
-	public int getBloodGroup() {
+	public BloodGroup getBloodGroup() {
 		return bloodGroup;
 	}
 
@@ -70,7 +70,7 @@ public class Human implements Alive {
 	}
 
 	public void printSize() {
-		System.out.println("Рост: " + size[0] + " Вес: " + size[1]);
+		System.out.println("Рост: " + size.height + " Вес: " + size.weight);
 	}
 
 	public String getPosition() {
