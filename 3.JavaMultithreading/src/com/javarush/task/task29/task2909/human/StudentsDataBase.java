13d0cdb792939c8aa2b37738caebd988034c5993
Taly <talyrus@gmail.com>
2017-11-20 09:26:41 +0300

    Решения задач курса JavaRush 2.0 (Big Task - Refactoring 8)

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/StudentsDataBase.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/StudentsDataBase.java
@@ -4,36 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StudentsDataBase {
-    public static List<Student> students = new ArrayList<>();
+	public static List<Student> students = new ArrayList<>();
 
-    public static void addInfoAboutStudent(Student student) {
-        students.add(student);
-        printInfoAboutStudent(student);
+	public static void addInfoAboutStudent(Student student) {
+		students.add(student);
+		printInfoAboutStudent(student);
 
-    }
+	}
 
-    public static void printInfoAboutStudent(Student student) {
-        System.out.println("Имя: " + student.name + " Возраст: " + student.getAge());
-    }
+	public static void printInfoAboutStudent(Student student) {
+		System.out.println("Имя: " + student.name + " Возраст: " + student.getAge());
+	}
 
-    public static void removeStudent(int index) throws IndexOutOfBoundsException {
-        students.remove(index);
-    }
+	public static void removeStudent(int index) {
+		if (students.size() > index && index >= 0) {
+			students.remove(index);
+		}
 
-    public static void findDimaOrSasha() {
-        boolean found = false;
-        for (int i = 0; i < students.size(); i++) {
-            if (!found) {
-                if (students.get(i).getName().equals("Dima")) {
-                    System.out.println("Студент Dima есть в базе.");
-                    found = true;
-                }
+	}
 
-                if (students.get(i).getName().equals("Sasha")) {
-                    System.out.println("Студент Sasha есть в базе.");
-                    found = true;
-                }
-            }
-        }
-    }
+	public static void findDimaOrSasha() {
+		for (int i = 0; i < students.size(); i++) {
+			if (students.get(i).getName().equals("Dima")) {
+				System.out.println("Студент Dima есть в базе.");
+				break;
+			}
+
+			if (students.get(i).getName().equals("Sasha")) {
+				System.out.println("Студент Sasha есть в базе.");
+				break;
+			}
+		}
+	}
 }
