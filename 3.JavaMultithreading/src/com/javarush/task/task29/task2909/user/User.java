1df81f480fbd1ec53ffb36acc14538ba7cb6c06f
Taly <talyrus@gmail.com>
2017-11-20 17:09:32 +0300

    Решения задач курса JavaRush 2.0 (Big Task - Refactoring 13)

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
@@ -11,6 +11,18 @@ public class User {
 
     private Work work;
 
+    public void printInfo() {
+        System.out.println("Имя: " + getName());
+        System.out.println("Фамилия: " + getSurname());
+    }
+
+    public void printAdditionalInfo() {
+        if (getAge() < 16)
+            System.out.println("Пользователь моложе 16 лет");
+        else
+            System.out.println("Пользователь старше 16 лет");
+    }
+
     public User(String name, String surname, int age) {
         this.name = name;
         this.surname = surname;
