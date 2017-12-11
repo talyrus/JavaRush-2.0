18df4f8fa474cef0e77e20d7b7f4adc890fb8041
Taly <talyrus@gmail.com>
2017-12-11 14:33:07 +0300

    Решения задач курса JavaRush 2.0 (Big Task - Chat 4)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Message.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Message.java
@@ -1,7 +1,29 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.Serializable;
+
 /**
  * Created by Taly on 08.12.2017.
  */
-public class Message {
+public class Message implements Serializable {
+	final private MessageType type;
+	final private String data;
+
+	public MessageType getType() {
+		return type;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public Message(MessageType type) {
+		this.type = type;
+		this.data = null;
+	}
+
+	public Message(MessageType type, String data) {
+		this.type = type;
+		this.data = data;
+	}
 }
