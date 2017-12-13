d57019d416dccffab11b5c0e870ffbbeff1cdce6
Taly <talyrus@gmail.com>
2017-12-13 13:52:43 +0300

    Решения задач курса JavaRush 2.0 (Big Task - Chat 13)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,6 +1,11 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.IOException;
 
 /**
  * Created by Taly on 13.12.2017.
@@ -14,5 +19,40 @@ public class Client {
 	//поток для установки сокетного соединения и чтения сообщений сервера
 	public class SocketThread extends Thread {
 
+	}
+
+	protected String getServerAddress() {
+		System.out.println("Введите адрес сервера: ");
+		String address = ConsoleHelper.readString();
+		return address;
+	}
+
+	protected int getServerPort() {
+		System.out.println("Введите порт сервера: ");
+		int port = ConsoleHelper.readInt();
+		return port;
+	}
+
+	protected String getUserName() {
+		System.out.println("Введите имя пользователя: ");
+		String userName = ConsoleHelper.readString();
+		return userName;
+	}
+
+	protected boolean shouldSendTextFromConsole() {
+		return true;
+	}
+
+	protected SocketThread getSocketThread() {
+		return new SocketThread();
+	}
+
+	protected void sendTextMessage(String text) {
+		try {
+			connection.send(new Message(MessageType.TEXT, text));
+		} catch (IOException e) {
+			ConsoleHelper.writeMessage("Ошибка отправки сообщения!");
+			clientConnected = false;
+		}
 	}
 }
