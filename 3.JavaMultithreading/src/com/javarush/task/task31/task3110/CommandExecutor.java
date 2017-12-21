88ec452477d8b3c679ee87018baca674cb9a8974
Taly <talyrus@gmail.com>
2017-12-21 10:45:27 +0300

    Решения задач курса JavaRush 2.0 (Big task - Archiver (7))

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/CommandExecutor.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/CommandExecutor.java
@@ -5,26 +5,22 @@ import com.javarush.task.task31.task3110.command.*;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by Taly on 21.12.2017.
- */
 public class CommandExecutor {
-	private static final Map<Operation, Command> allKnownCommandsMap = new HashMap<>();
+    private static final Map<Operation, Command> allKnownCommandsMap = new HashMap<>();
 
-	static { // инициализируем карту
-		allKnownCommandsMap.put(Operation.ADD, new ZipAddCommand());
-		allKnownCommandsMap.put(Operation.CONTENT, new ZipContentCommand());
-		allKnownCommandsMap.put(Operation.CREATE, new ZipCreateCommand());
-		allKnownCommandsMap.put(Operation.EXTRACT, new ZipExtractCommand());
-		allKnownCommandsMap.put(Operation.REMOVE, new ZipRemoveCommand());
-		allKnownCommandsMap.put(Operation.EXIT, new ExitCommand());
-	}
+    static {
+        allKnownCommandsMap.put(Operation.CREATE, new ZipCreateCommand());
+        allKnownCommandsMap.put(Operation.ADD, new ZipAddCommand());
+        allKnownCommandsMap.put(Operation.REMOVE, new ZipRemoveCommand());
+        allKnownCommandsMap.put(Operation.EXTRACT, new ZipExtractCommand());
+        allKnownCommandsMap.put(Operation.CONTENT, new ZipContentCommand());
+        allKnownCommandsMap.put(Operation.EXIT, new ExitCommand());
+    }
 
-	private CommandExecutor() { // запрет явного вызова конструктора
+    private CommandExecutor() {
+    }
 
-	}
-
-	public static void execute(Operation operation) throws Exception {
-		allKnownCommandsMap.get(operation).execute(); //находим нужную команду и запускаем у нее метод execute()
-	}
+    public static void execute(Operation operation) throws Exception {
+        allKnownCommandsMap.get(operation).execute();
+    }
 }
---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/exception/PathIsNotFoundException.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task31.task3110.exception;
+
+/**
+ * Created by Taly on 21.12.2017.
+ */
+public class PathIsNotFoundException extends Exception {
+}
