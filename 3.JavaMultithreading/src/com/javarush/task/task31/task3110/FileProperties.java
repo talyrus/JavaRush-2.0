cf7cfd37cfe827c76761fa29434547d996c9275e
Taly <talyrus@gmail.com>
2017-12-22 13:05:07 +0300

    Решения задач курса JavaRush 2.0 (Big task - Archiver (13))

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileProperties.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileProperties.java
@@ -1,51 +1,55 @@
 package com.javarush.task.task31.task3110;
 
-/**
- * Created by Taly on 22.12.2017.
- */
 public class FileProperties {
-	private String name;
-	private long size; //размер архивируемого файла
-	private long compressedSize;    // размер после сжатия в байтах
-	private int compressionMethod; // метод сжатия
+    private String name;
+    private long size;
+    private long compressedSize;
+    private int compressionMethod;
 
-	public String getName() {
-		return name;
-	}
+    public FileProperties(String name, long size, long compressedSize, int compressionMethod) {
+        this.name = name;
+        this.size = size;
+        this.compressedSize = compressedSize;
+        this.compressionMethod = compressionMethod;
+    }
 
-	public long getSize() {
-		return size;
-	}
 
-	public long getCompressedSize() {
-		return compressedSize;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public int getCompressionMethod() {
-		return compressionMethod;
-	}
+    public long getSize() {
+        return size;
+    }
 
-	public FileProperties(String name, long size, long compressedSize, int compressionMethod) {
-		this.name = name;
-		this.size = size;
-		this.compressedSize = compressedSize;
-		this.compressionMethod = compressionMethod;
-	}
+    public long getCompressedSize() {
+        return compressedSize;
+    }
 
-	public long getCompressionRatio() {
-		return 100 - ((compressedSize * 100) / size);
-	}
+    public int getCompressionMethod() {
+        return compressionMethod;
+    }
 
-	@Override
-	public String toString() {
-		StringBuilder stringBuilder = new StringBuilder();
-		stringBuilder.append(name);
-		if (size > 0) {
-			stringBuilder.append("\t");
-			stringBuilder.append(size / 1024 + " Kb (");
-			stringBuilder.append(compressedSize / 1024 + " Kb) сжатие: ");
-			stringBuilder.append(getCompressionRatio() + "%");
-		}
-		return stringBuilder.toString();
-	}
+    public long getCompressionRatio() {
+        // Вычисляем степень сжатия
+        return 100 - ((compressedSize * 100) / size);
+    }
+
+    @Override
+    public String toString() {
+        // Строим красивую строку из свойств
+        StringBuilder builder = new StringBuilder();
+        builder.append(name);
+        if (size > 0) {
+            builder.append("\t");
+            builder.append(size / 1024);
+            builder.append(" Kb (");
+            builder.append(compressedSize / 1024);
+            builder.append(" Kb) сжатие: ");
+            builder.append(getCompressionRatio());
+            builder.append("%");
+        }
+
+        return builder.toString();
+    }
 }
