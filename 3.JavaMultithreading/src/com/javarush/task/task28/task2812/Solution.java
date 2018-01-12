55185eccebb0ac18725a889d2ed44deb09130bee
Taly <talyrus@gmail.com>
2018-01-12 13:22:01 +0300

    Решения задач курса JavaRush 2.0 (ShutdownNow)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2812/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task28.task2812;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/* 
+ShutdownNow!
+*/
+
+public class Solution {
+	public static void main(String[] args) throws InterruptedException {
+		ExecutorService executor = Executors.newFixedThreadPool(5);
+		for (int i = 1; i <= 10; i++) {
+			final int localId = i;
+			executor.submit(new Runnable() {
+				public void run() {
+					doExpensiveOperation(localId);
+				}
+			});
+		}
+
+		final List<Runnable> list = executor.shutdownNow();
+		for (Object el : list) {
+			System.out.println(el + " was not completed");
+		}
+	}
+
+	private static void doExpensiveOperation(int localId) {
+		System.out.println(Thread.currentThread().getName() + ", localId=" + localId);
+	}
+}
