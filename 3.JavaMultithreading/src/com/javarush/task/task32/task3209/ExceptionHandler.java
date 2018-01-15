8d640835b45b3dfa61f72342443187ea0eba5279
Taly <talyrus@gmail.com>
2018-01-15 16:52:39 +0300

    Решения задач курса JavaRush 2.0 (HTML Editor - Big-6)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/ExceptionHandler.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/ExceptionHandler.java
@@ -4,7 +4,7 @@ package com.javarush.task.task32.task3209;
  * Created by Taly on 15.01.2018.
  */
 public class ExceptionHandler {
-	public static void log(Exception e) {
+	public static void log(Exception e){
 		System.out.println(e.toString());
 	}
 }
--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -1,8 +1,10 @@
 package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
+import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -43,7 +45,20 @@ public class View extends JFrame implements ActionListener {
 	}
 
 	public void initEditor() { //инициализация панелей редактора
+		htmlTextPane.setContentType("text/html"); // Устанавили значение "text/html" в качестве типа контента для компонента htmlTextPane.
+		JScrollPane jScrollPaneHtml = new JScrollPane(htmlTextPane); //Создали новый локальный компонент JScrollPane на базе htmlTextPane.
+		tabbedPane.addTab("HTML", jScrollPaneHtml); //добавили его во вкладку в панель tabbedPane с именем "HTML"
 
+		JScrollPane jScrollPaneText = new JScrollPane(plainTextPane); //Создали новый локальный компонент JScrollPane на базе plainTextPane.
+		tabbedPane.addTab("Текст", jScrollPaneText); //Добавили еще одну вкладку в tabbedPane с именем "Текст" и вторым компонентом
+
+		tabbedPane.setPreferredSize(new Dimension(1024, 768));// Устанавили предпочтительный размер панели tabbedPane.
+		//Создали объект класса TabbedPaneChangeListener
+		TabbedPaneChangeListener tabbedPaneChangeListener = new TabbedPaneChangeListener(this);
+		// и установили его в качестве слушателя изменений в tabbedPane.
+		tabbedPane.addChangeListener(tabbedPaneChangeListener);
+		// Добавили по центру панели контента текущего фрейма нашу панель с вкладками.
+		this.getContentPane().add(tabbedPane, BorderLayout.CENTER);
 	}
 
 	public void initGui() { //инициализация графического интерфейса
