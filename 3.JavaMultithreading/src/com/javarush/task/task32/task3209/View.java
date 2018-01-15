06102dae02a652e84c209dd27db83b2bd7dc347c
Taly <talyrus@gmail.com>
2018-01-15 11:23:05 +0300

    Решения задач курса JavaRush 2.0 (HTML Editor - Big-1)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task32.task3209;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Taly on 15.01.2018.
+ */
+public class View extends JFrame implements ActionListener {
+	private Controller controller;
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+	}
+
+	public Controller getController() {
+		return controller;
+	}
+
+	public void setController(Controller controller) {
+		this.controller = controller;
+	}
+
+
+}
