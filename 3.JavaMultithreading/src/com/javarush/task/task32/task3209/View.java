f6b025b9a72e4edd531e1cffc641249b6dbf5afe
Taly <talyrus@gmail.com>
2018-01-17 16:10:54 +0300

    Решения задач курса JavaRush 2.0 (HTML Editor - Big-13)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -132,5 +132,8 @@ public class View extends JFrame implements ActionListener {
 		undoManager.discardAllEdits();
 	}
 
+	public boolean isHtmlTabSelected() {
+		return tabbedPane.getSelectedIndex() == 0;
+	}
 
 }
--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/listeners/TextEditMenuListener.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/listeners/TextEditMenuListener.java
@@ -2,8 +2,10 @@ package com.javarush.task.task32.task3209.listeners;
 
 import com.javarush.task.task32.task3209.View;
 
+import javax.swing.*;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
+import java.awt.*;
 
 /**
  * Created by Taly on 16.01.2018.
@@ -12,7 +14,14 @@ public class TextEditMenuListener implements MenuListener {
 	private View view;
 
 	@Override
-	public void menuSelected(MenuEvent e) {
+	public void menuSelected(MenuEvent menuEvent) {
+		// из переданного параметра получим обект, над которым будем совершать действия - Меню
+		JMenu menu = (JMenu) menuEvent.getSource();
+		// создадим массив компонентов и получим содержимое меню (все пункты)
+		Component[] elements = menu.getMenuComponents();
+		for (Component el : elements) { //переберм все элементы массива и установим значение
+			el.setEnabled(view.isHtmlTabSelected());
+		}
 
 	}
 
