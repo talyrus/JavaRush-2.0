478a4f861c12579d42d62f97664e09b686416ded
Taly <talyrus@gmail.com>
2018-01-17 17:10:43 +0300

    Решения задач курса JavaRush 2.0 (HTML Editor - Big-15)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task32.task3209;
 
 import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
 
 /**
@@ -32,5 +33,18 @@ public class Controller {
 
 	public HTMLDocument getDocument() {
 		return document;
+	}
+
+	public void resetDocument() { //сброс текущего документа
+		if (document != null) { // если документ существует
+			//сбросить у него слушателя правок, которые можно отменить/вернуть
+			document.removeUndoableEditListener(view.getUndoListener());
+		}
+		HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
+		//создадим новый документ
+		HTMLDocument document = (HTMLDocument) htmlEditorKit.createDefaultDocument();
+		//добавим ему слушателя правок
+		document.addUndoableEditListener(view.getUndoListener());
+		view.update(); //вызовем у представления метод update()
 	}
 }
