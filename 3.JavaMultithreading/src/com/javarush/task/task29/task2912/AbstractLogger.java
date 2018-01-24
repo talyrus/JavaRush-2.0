2870358523696700f14b406c601d0bd765ffe1c8
Taly <talyrus@gmail.com>
2018-01-24 13:26:07 +0300

    Решения задач курса JavaRush 2.0 (Refactoring)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2912/AbstractLogger.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task29.task2912;
+
+/**
+ * Created by Taly on 24.01.2018.
+ */
+public abstract class AbstractLogger implements Logger {
+	int level;
+	Logger next;
+
+	public void inform(String message, int level) {
+		if (this.level <= level) {
+			info(message);
+		}
+		if (next != null) {
+			next.inform(message, level);
+		}
+	}
+
+	public void setNext(Logger next) {
+		this.next = next;
+	}
+
+}
