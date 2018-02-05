ab933be56b331097bd427a1831d939a4d9fa889a
Taly <talyrus@gmail.com>
2018-02-05 15:03:42 +0300

    Решения задач курса JavaRush 2.0 (Bitwise operations)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3013/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task30.task3013;
+
+/* 
+Набираем код
+*/
+
+public class Solution {
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		int number = Integer.MAX_VALUE - 133;
+		System.out.println(Integer.toString(number, 2));
+
+		String result = Integer.toString(solution.resetLowerBits(number), 2);
+		System.out.println(result);
+	}
+
+	public int resetLowerBits(int number) {
+		// сдвинули биты вправо на n позицию,
+		// для каждого бита применим операцию НЕ, заменяя его на обратный
+		// применим побитовое И с присвоением
+		number &= ~(number >> 1);
+		number &= ~(number >> 2);
+		number &= ~(number >> 4);
+		number &= ~(number >> 8);
+		number &= ~(number >> 16);
+		return number;
+	}
+}
