f2c38895f3d5c010f53a4d38883c2fd368e691bc
Taly <talyrus@gmail.com>
2018-02-06 11:49:18 +0300

    Решения задач курса JavaRush 2.0 (2048 - Big-3)

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -4,4 +4,15 @@ package com.javarush.task.task35.task3513;
  * Created by Taly on 06.02.2018.
  */
 public class Model { //будет содержать игровую логику и хранить игровое поле.
+	private static final int FIELD_WIDTH = 4; //определяющая ширину игрового поля.
+	private Tile[][] gameTiles; //Приватный двумерный массив состоящий из объектов класса Tile.
+
+	public Model() { //инициализирующий игровое поле и заполняющий его пустыми плитками.
+		gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+		for (int i = 0; i < FIELD_WIDTH; i++) {
+			for (int j = 0; j < FIELD_WIDTH; j++) {
+				gameTiles[i][j] = new Tile();
+			}
+		}
+	}
 }
