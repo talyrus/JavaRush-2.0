ed4b0b128b1bde9d911042256b4f9ec7ba9cbdc5
Taly <talyrus@gmail.com>
2018-02-06 09:04:17 +0300

    Решения задач курса JavaRush 2.0 (2048 - Big-1)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task35.task3513;
+
+/**
+ * Created by Taly on 06.02.2018.
+ */
+public class Controller { //будет следить за нажатием клавиш во время игры.
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Main.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task35.task3513;
+
+/**
+ * Created by Taly on 06.02.2018.
+ */
+public class Main { //будет содержать только метод main и служить точкой входа в наше приложение.
+	public static void main(String[] args) {
+
+	}
+}
