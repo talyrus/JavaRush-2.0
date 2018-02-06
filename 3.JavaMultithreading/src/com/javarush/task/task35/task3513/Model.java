3f0c4c11d26f72befef7a79136f928f5ba3a134e
Taly <talyrus@gmail.com>
2018-02-06 17:28:45 +0300

    Решения задач курса JavaRush 2.0 (2048 - Big-5)

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -9,9 +9,13 @@ import java.util.List;
 public class Model { //будет содержать игровую логику и хранить игровое поле.
 	private static final int FIELD_WIDTH = 4; //определяющая ширину игрового поля.
 	private Tile[][] gameTiles; //Приватный двумерный массив состоящий из объектов класса Tile.
+	protected int score;    //хранит текущий счет
+	protected int maxTile;  // хранит максимальный вес плитки на игровом поле
 
 	public Model() { //инициализирующий игровое поле и заполняющий его пустыми плитками.
 		resetGameTiles();
+		score = 0;
+		maxTile = 2;
 	}
 
 	private void addTile() { //должен изменять значение случайной пустой плитки в массиве gameTiles на 2 или 4 с вероятностью 0.9 и 0.1 соответственно.
@@ -44,5 +48,43 @@ public class Model { //будет содержать игровую логику
 		}
 		addTile();
 		addTile();
+	}
+
+	private void compressTiles(Tile[] tiles) { //метод сжатия плиток
+		for (int i = 0; i < FIELD_WIDTH - 1; i++) {
+			for (int j = 0; j < FIELD_WIDTH - 1; j++) {
+				if (tiles[j].isEmpty() && !tiles[j + 1].isEmpty()) {
+					Tile temp = tiles[j];
+					tiles[j] = tiles[j + 1];
+					tiles[j + 1] = temp;
+				}
+			}
+		}
+	}
+
+	private void mergeTiles(Tile[] tiles) { // метод слияния плиток одного номинала
+		boolean isChanged = false;
+		for (int j = 0; j < 3; j++) {
+			if (!tiles[j].isEmpty() && tiles[j].value == tiles[j + 1].value) {
+				tiles[j].value *= 2;
+				tiles[j + 1].value = 0;
+				if (tiles[j].value > maxTile)
+					maxTile = tiles[j].value;
+				score += tiles[j].value;
+				isChanged = true;
+
+			}
+		}
+
+		if (isChanged) {
+			Tile temp;
+			for (int j = 0; j < 3; j++) {
+				if (tiles[j].isEmpty() && !tiles[j + 1].isEmpty()) {
+					temp = tiles[j];
+					tiles[j] = tiles[j + 1];
+					tiles[j + 1] = temp;
+				}
+			}
+		}
 	}
 }
