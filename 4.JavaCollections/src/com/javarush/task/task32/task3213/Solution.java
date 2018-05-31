e466cfdd0c10cad3c0f1abbc80a492bc238f74f6
Taly <talyrus@gmail.com>
2018-05-31 12:29:59 +0300

    Решения задач курса JavaRush 2.0

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3213/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task32.task3213;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+/* 
+Шифр Цезаря
+*/
+
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		StringReader reader = new StringReader("Khoor#Dpljr#&C,₷B'3");
+		System.out.println(decode(reader, -3));  //Hello Amigo #@)₴?$0
+	}
+
+	public static String decode(StringReader reader, int key) throws IOException {
+		int simbol;
+		StringBuffer buffer = new StringBuffer();
+		try {
+			while ((simbol = reader.read()) != -1) {
+				buffer.append(Character.toString((char) (simbol + key)));
+			}
+		} catch (Exception e) {
+			return new String();
+		}
+		return buffer.toString();
+	}
+}
