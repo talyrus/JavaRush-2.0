09ed190836f6c42df52db552f0b0c2b56d10b494
Taly <talyrus@gmail.com>
2018-06-01 14:15:44 +0300

    Решения задач курса JavaRush 2.0

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3205/CustomInvocationHandler.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task32.task3205;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * Created by Taly on 01.06.2018.
+ */
+public class CustomInvocationHandler implements InvocationHandler {
+	private SomeInterfaceWithMethods obj;
+
+	public CustomInvocationHandler(SomeInterfaceWithMethods siwm) {
+		obj = siwm;
+	}
+
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		System.out.println(method.getName() + " in");
+		Object res = method.invoke(obj, args);
+		System.out.println(method.getName() + " out");
+		return res;
+	}
+}
