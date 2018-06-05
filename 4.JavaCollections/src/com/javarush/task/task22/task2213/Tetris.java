0137a0b0d9eb6ef9634a51edc3f7aa434ad6eb30
Taly <talyrus@gmail.com>
2018-06-05 11:39:29 +0300

    Решения задач курса JavaRush 2.0 (Big task - Tetris)

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -4,7 +4,18 @@ package com.javarush.task.task22.task2213;
  * Created by Taly on 05.06.2018.
  */
 public class Tetris {
+	private Field field;
+	private Figure figure;
+
 	public static void main(String[] args) {
 
+	}
+
+	public Field getField() {
+		return field;
+	}
+
+	public Figure getFigure() {
+		return figure;
 	}
 }
