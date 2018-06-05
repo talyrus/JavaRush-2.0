cedb8e1ff93917feaa2b8df507cabbd5efcbe0b6
Taly <talyrus@gmail.com>
2018-06-05 12:04:16 +0300

    Решения задач курса JavaRush 2.0 (Big task - Tetris)

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
@@ -25,4 +25,22 @@ public class Field {
 		this.height = height;
 		matrix = new int[height][width];
 	}
+
+	public void print() { //объект будет отрисовывать на экран свое текущее состояние
+
+	}
+
+	public void removeFullLines() { // будет удалять из матрицы полностью заполненные строки и сдвигать вышележащие строки вниз
+
+	}
+
+	public Integer getValue(int x, int y) { //возвращает значение которое находится в матрице с координатами x и y
+		return null;
+	}
+
+	public void setValue(int x, int y, int value) { //устанавливает переданное значение в ячейку массива (матрицы) с координатами x, y
+		
+	}
+
+
 }
