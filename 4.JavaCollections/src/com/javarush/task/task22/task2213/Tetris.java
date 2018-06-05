d2d218cc643f900cc7807f9ef76d07edd7716af6
Taly <talyrus@gmail.com>
2018-06-05 11:47:26 +0300

    Решения задач курса JavaRush 2.0 (Big task - Tetris)

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -6,9 +6,11 @@ package com.javarush.task.task22.task2213;
 public class Tetris {
 	private Field field;
 	private Figure figure;
+	public static Tetris game;
 
 	public static void main(String[] args) {
-
+		game = new Tetris();
+		game.run();
 	}
 
 	public Field getField() {
