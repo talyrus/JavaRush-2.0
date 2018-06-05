c9a24dcc5f1eef67041fd4b65c662d0baf69e7a8
Taly <talyrus@gmail.com>
2018-06-05 14:18:29 +0300

    Решения задач курса JavaRush 2.0 (Big task - Tetris)

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
@@ -27,19 +27,27 @@ public class Figure {
 	}
 
 	public void left() { //для движения фигурки влево
-
+		if (isCurrentPositionAvailable()) {
+			x--;
+		}
 	}
 
 	public void right() { //для движения фигурки вправо
-
+		if (isCurrentPositionAvailable()) {
+			x++;
+		}
 	}
 
 	public void down() { //для движения фигурки вниз.
-
+		if (isCurrentPositionAvailable()) {
+			y++;
+		}
 	}
 
 	public void up() { //для движения фигурки вверх.
-
+		if (isCurrentPositionAvailable()) {
+			y--;
+		}
 	}
 
 	public void rotate() { //для поворота фигурки вокруг главной диагонали.
