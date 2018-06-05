24e85583ab3ad1061a3a05a2d78a3a3a0b572946
Taly <talyrus@gmail.com>
2018-06-05 12:18:42 +0300

    Решения задач курса JavaRush 2.0 (Big task - Tetris)

---
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/FigureFactory.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task22.task2213;
+
+/**
+ * Created by Taly on 05.06.2018.
+ */
+public class FigureFactory { // создавать фигуры различных форм
+	public static Figure createRandomFigure(int x, int y) {
+		return null;
+	}
+}
