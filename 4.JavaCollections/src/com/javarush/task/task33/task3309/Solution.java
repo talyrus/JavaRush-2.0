b811c3eae2a0fede651ba7fb8ed322fb1839c496
Taly <talyrus@gmail.com>
2018-06-25 17:23:29 +0300

    Решения задач курса JavaRush 2.0

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3309/Solution.java
@@ -0,0 +1,68 @@
+package com.javarush.task.task33.task3309;
+
+import org.w3c.dom.Comment;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.StringWriter;
+import java.util.regex.Pattern;
+
+/*
+Комментарий внутри xml
+*/
+public class Solution {
+	public static String toXmlWithComment(Object obj, String tagName, String comment) {
+		try {
+			JAXBContext context = JAXBContext.newInstance(obj.getClass());
+			Marshaller marshaller = context.createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+			Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+			marshaller.marshal(obj, doc);
+			NodeList nodes = doc.getElementsByTagName("*");
+			for (int i = 0; i < nodes.getLength(); i++) {
+				Node node = nodes.item(i);
+				if (node.getNodeName().equals(tagName)) {
+					Comment comm = doc.createComment(comment);
+					node.getParentNode().insertBefore(comm, node);
+				}
+				replaceTextWithCDATA(node, doc);
+			}
+			Transformer transformer = TransformerFactory.newInstance().newTransformer();
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty("{http://xml.apache.org/xslt}intent-amount", "4");
+			StringWriter stringWriter = new StringWriter();
+			transformer.transform(new DOMSource(doc), new StreamResult(stringWriter));
+			return stringWriter.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	private static void replaceTextWithCDATA(Node node, Document doc) {
+		if (node.getNodeType() == 3 && (Pattern.compile("[<>&'\"]").matcher(node.getTextContent()).find())) {
+			Node cnode = doc.createCDATASection(node.getNodeValue());
+			node.getParentNode().replaceChild(cnode, node);
+		}
+		NodeList nodeList = node.getChildNodes();
+		for (int j = 0; j < nodeList.getLength(); j++) {
+			replaceTextWithCDATA(nodeList.item(j), doc);
+		}
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+
+}
