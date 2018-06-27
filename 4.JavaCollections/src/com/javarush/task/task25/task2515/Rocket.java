834aaaa32ecd8b36a8866f7da4ad69a4053d49e0
Taly <talyrus@gmail.com>
2018-06-27 14:16:00 +0300

    Решения задач курса JavaRush 2.0 (Big task - Space)

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
@@ -1,26 +1,27 @@
 package com.javarush.task.task25.task2515;
 
 /**
- * Created by Taly on 26.06.2018.
+ * Класс для объектов-ракета
  */
 public class Rocket extends BaseObject {
-	public Rocket(double x, double y) {
-		super(x, y, 1);
-	}
 
-	/**
-	 * Отрисовываем себя на холсте.
-	 */
-	@Override
-	public void draw(Canvas canvas) {
-		canvas.setPoint(x, y, 'R');
-	}
+    public Rocket(double x, double y) {
+        super(x, y, 1);
+    }
 
-	/**
-	 * Двигаем себя вниз на один ход.
-	 */
-	@Override
-	public void move() {
-		y--;
-	}
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.setPoint(x, y, 'R');
+    }
+
+    /**
+     * Двигаем себя вверх на один ход.
+     */
+    @Override
+    public void move() {
+        y--;
+    }
 }
--- 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
@@ -4,7 +4,17 @@ package com.javarush.task.task25.task2515;
  * Created by Taly on 26.06.2018.
  */
 public class SpaceShip extends BaseObject {
-	public SpaceShip(double x, double y, double radius) {
-		super(x, y, radius);
+	private double dx = 0;
+
+	public SpaceShip(double x, double y) {
+		super(x, y, 3);
+	}
+
+	public void moveLeft() {
+		dx = -1;
+	}
+
+	public void moveRight() {
+		dx = 1;
 	}
 }
