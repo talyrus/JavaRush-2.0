a132a1813759bf48a26d41760ce8d12594ec6c1e
Taly <talyrus@gmail.com>
2018-06-27 11:32:08 +0300

    Решения задач курса JavaRush 2.0 (Big task - Space)

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -23,6 +23,24 @@ public class Canvas {
 	public Canvas(int width, int height) {
 		this.width = width;
 		this.height = height;
-		matrix = new char[height][width];
+		matrix = new char[height + 2][width + 2];
+	}
+
+	public void setPoint(double x, double y, char c) {
+		int xx = (int) Math.round(x);
+		int yy = (int) Math.round(y);
+		if (x > 0 && y > 0 && y <= matrix.length && x <= matrix[0].length) {
+			matrix[yy][xx] = c;
+		}
+	}
+
+	public void drawMatrix(double x, double y, int[][] matrix, char c) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				if (matrix[i][j] != 0) {
+					setPoint(x + j, y + i, c);
+				}
+			}
+		}
 	}
 }
