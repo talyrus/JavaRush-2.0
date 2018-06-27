948bcecbb74cdd5c17bfb61c8b88c6cc118bb0cb
Taly <talyrus@gmail.com>
2018-06-27 12:00:46 +0300

    Решения задач курса JavaRush 2.0 (Big task - Space)

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Bomb.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Bomb.java
@@ -4,7 +4,16 @@ package com.javarush.task.task25.task2515;
  * Created by Taly on 26.06.2018.
  */
 public class Bomb extends BaseObject {
-	public Bomb(double x, double y, double radius) {
-		super(x, y, radius);
+	public Bomb(double x, double y) {
+		super(x, y, 1);
+	}
+
+	public void move() {
+		y++;
+	}
+
+	@Override
+	public void draw(Canvas canvas) {
+		canvas.setPoint(x, y, 'B');
 	}
 }
--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -1,61 +1,66 @@
 package com.javarush.task.task25.task2515;
 
-/**
- * Created by Taly on 26.06.2018.
- */
+
 public class Canvas {
-	private int width;
-	private int height;
-	private char[][] matrix;
+    private int width;
+    private int height;
+    private char[][] matrix;
 
-	public int getWidth() {
-		return width;
-	}
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.matrix = new char[height + 2][width + 2];
+    }
 
-	public int getHeight() {
-		return height;
-	}
+    public void clear() {
+        this.matrix = new char[height + 2][width + 2];
+    }
 
-	public char[][] getMatrix() {
-		return matrix;
-	}
+    public void drawMatrix(double x, double y, int[][] matrix, char c) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] != 0) {
+                    setPoint(x + j, y + i, c);
+                }
+            }
+        }
+    }
 
-	public Canvas(int width, int height) {
-		this.width = width;
-		this.height = height;
-		matrix = new char[height + 2][width + 2];
-	}
+    public void setPoint(double x, double y, char c) {
+        int xRounded = (int) Math.round(x);
+        int yRounded = (int) Math.round(y);
+        if (xRounded >= 0 && xRounded < matrix[0].length && yRounded >= 0 && yRounded < matrix.length) {
+            matrix[yRounded][xRounded] = c;
+        }
+    }
 
-	public void setPoint(double x, double y, char c) {
-		int xx = (int) Math.round(x);
-		int yy = (int) Math.round(y);
-		if (x > 0 && y > 0 && y <= matrix.length && x <= matrix[0].length) {
-			matrix[yy][xx] = c;
-		}
-	}
+    public void print() {
+        System.out.println();
 
-	public void drawMatrix(double x, double y, int[][] matrix, char c) {
-		for (int i = 0; i < matrix.length; i++) {
-			for (int j = 0; j < matrix[0].length; j++) {
-				if (matrix[i][j] != 0) {
-					setPoint(x + j, y + i, c);
-				}
-			}
-		}
-	}
+        for (int i = 0; i < height + 2; i++) {
+            for (int j = 0; j < width + 2; j++) {
+                System.out.print(" ");
+                System.out.print(matrix[i][j]);
+                System.out.print(" ");
+            }
 
-	public void clear() {
-		this.matrix = new char[height + 2][width + 2];
-	}
+            System.out.println();
+        }
 
-	public void print() {
-		for (int i = 0; i < height; i++) {
-			for (int j = 0; j < width; j++) {
-				System.out.print(matrix[i][j]);
-			}
-			System.out.println();
-		}
-		System.out.println();
-		System.out.println();
-	}
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
+    }
 }
