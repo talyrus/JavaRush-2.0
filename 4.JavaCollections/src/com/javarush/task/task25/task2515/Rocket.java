57c6efce487da81e97cf618d579aa4f98088b4e0
Taly <talyrus@gmail.com>
2018-06-27 13:47:13 +0300

    Решения задач курса JavaRush 2.0 (Big task - Space)

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
@@ -4,7 +4,23 @@ package com.javarush.task.task25.task2515;
  * Created by Taly on 26.06.2018.
  */
 public class Rocket extends BaseObject {
-	public Rocket(double x, double y, double radius) {
-		super(x, y, radius);
+	public Rocket(double x, double y) {
+		super(x, y, 1);
+	}
+
+	/**
+	 * Отрисовываем себя на холсте.
+	 */
+	@Override
+	public void draw(Canvas canvas) {
+		canvas.setPoint(x, y, 'R');
+	}
+
+	/**
+	 * Двигаем себя вниз на один ход.
+	 */
+	@Override
+	public void move() {
+		y--;
 	}
 }
