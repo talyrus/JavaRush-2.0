92e5f47384707c94002b955aa11683c54c9d0747
Taly <talyrus@gmail.com>
2018-06-27 10:40:59 +0300

    Решения задач курса JavaRush 2.0 (Big task - Space)

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/BaseObject.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/BaseObject.java
@@ -1,62 +1,92 @@
 package com.javarush.task.task25.task2515;
 
 /**
- * Created by Taly on 26.06.2018.
+ * Базовый класс для всех объектов игры.
  */
 public abstract class BaseObject {
-	private double x;
-	private double y;
-	private double radius;
-	private boolean isAlive;
+    //координаты
+    protected double x;
+    protected double y;
+    //радиус объекта
+    protected double radius;
+    //состояние объект - жив ли объект
+    private boolean isAlive;
 
-	public double getX() {
-		return x;
-	}
+    public BaseObject(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.isAlive = true;
+    }
 
-	public void setX(double x) {
-		this.x = x;
-	}
+    public double getX() {
+        return x;
+    }
 
-	public double getY() {
-		return y;
-	}
+    public void setX(double x) {
+        this.x = x;
+    }
 
-	public void setY(double y) {
-		this.y = y;
-	}
+    public double getY() {
+        return y;
+    }
 
-	public double getRadius() {
-		return radius;
-	}
+    public void setY(double y) {
+        this.y = y;
+    }
 
-	public void setRadius(double radius) {
-		this.radius = radius;
-	}
+    public double getRadius() {
+        return radius;
+    }
 
-	public boolean isAlive() {
-		return isAlive;
-	}
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
 
-	public BaseObject(double x, double y, double radius) {
-		this.x = x;
-		this.y = y;
-		this.radius = radius;
-		isAlive = true;
-	}
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
+    public void draw(Canvas canvas) {
+        //do nothing
+    }
 
-	public void draw() {
+    /**
+     * Двигаем себя на один ход.
+     */
+    public void move() {
+        //do nothing
+    }
 
-	}
+    /**
+     * Проверяем - не выходит ли (x,y) за границы.
+     */
+    public void checkBorders(double minx, double maxx, double miny, double maxy) {
+        if (x < minx) x = minx;
+        if (x > maxx) x = maxx;
+        if (y < miny) y = miny;
+        if (y > maxy) y = maxy;
+    }
 
-	public void move() {
+    public boolean isAlive() {
+        return isAlive;
+    }
 
-	}
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
 
-	public void die() {
-		isAlive = false;
-	}
+    public void die() {
+        isAlive = false;
+    }
 
-	public boolean isIntersect(BaseObject o) {
-		return Math.hypot(this.getX() - o.getX(), this.getY() - o.getY()) < Math.max(this.getRadius(), o.getRadius());
-	}
+    /**
+     * Проверяем - пересекаются ли переданный(o) и наш(this) объекты.
+     */
+    public boolean isIntersect(BaseObject o) {
+        double dx = x - o.x;
+        double dy = y - o.y;
+        double destination = Math.sqrt(dx * dx + dy * dy);
+        double destination2 = Math.max(radius, o.radius);
+        return destination <= destination2;
+    }
 }
--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -4,4 +4,25 @@ package com.javarush.task.task25.task2515;
  * Created by Taly on 26.06.2018.
  */
 public class Canvas {
+	private int width;
+	private int height;
+	private char[][] matrix;
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public char[][] getMatrix() {
+		return matrix;
+	}
+
+	public Canvas(int width, int height) {
+		this.width = width;
+		this.height = height;
+		matrix = new char[height][width];
+	}
 }
