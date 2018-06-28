1927974ec1314238a1e50369c925d67eb379e045
Taly <talyrus@gmail.com>
2018-06-28 15:14:31 +0300

    Решения задач курса JavaRush 2.0

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3411/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task34.task3411;
+
+/* 
+Ханойские башни
+*/
+
+public class Solution {
+	public static void main(String[] args) {
+		int count = 3;
+		moveRing('A', 'B', 'C', count);
+	}
+
+	public static void moveRing(char a, char b, char c, int count) {
+		//напишите тут ваш код
+		if (count == 1) {
+			System.out.printf("from %c to %c \n", a, b);
+		} else if (count > 1) {
+			moveRing(a, c, b, count - 1);
+			System.out.printf("from %c to %c \n", a, b);
+			moveRing(c, b, a, count - 1);
+		}
+	}
+}
