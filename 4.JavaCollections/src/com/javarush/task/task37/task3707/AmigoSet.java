2ad2b7904f14ce983f77237842f82f4bee55e687
Taly <talyrus@gmail.com>
2018-07-10 14:44:31 +0300

    Решения задач курса JavaRush 2.0 - Big task: Collections

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,15 +1,26 @@
 package com.javarush.task.task37.task3707;
 
 import java.io.Serializable;
-import java.util.AbstractSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by Taly on 10.07.2018.
  */
 public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneable, Set<E> {
+	private static final Object PRESENT = new Object();
+	private transient HashMap<E, Object> map;
 
+	public AmigoSet() {
+		map = new HashMap<>();
+	}
+
+	public AmigoSet(Collection<? extends E> collection) {
+		int capacity = Math.max(16, (int) Math.ceil(collection.size() / .75f));
+		this.map = new HashMap<>(capacity);
+		for (E e : collection) {
+			map.put(e, PRESENT);
+		}
+	}
 
 	@Override
 	public Iterator<E> iterator() {
@@ -33,7 +44,8 @@ public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneab
 
 	@Override
 	public boolean add(E e) {
-		return super.add(e);
+		return map.put(e, PRESENT) == null;
+
 	}
 
 	@Override
