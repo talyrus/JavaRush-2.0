6ed8c10f6d4c75d016bba4021c108c508e3fec43
Taly <talyrus@gmail.com>
2018-08-02 12:57:10 +0300

    Решения задач курса JavaRush 2.0

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task33.task3310;
+
+/**
+ * Created by Taly on 02.08.2018.
+ */
+public class Shortener {
+	public Long getId(String string) {
+		return null;
+	}
+
+	public String getString(Long id) {
+		return null;
+	}
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task33.task3310;
+
+/**
+ * Created by Taly on 02.08.2018.
+ */
+public class Solution {
+	public static void main(String[] args) {
+
+	}
+}
