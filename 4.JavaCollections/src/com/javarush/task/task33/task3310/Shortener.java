2deaa0da191d936da0c3cb90d883699fe3732d21
Taly <talyrus@gmail.com>
2018-08-02 14:05:38 +0300

    Решения задач курса JavaRush 2.0

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -1,14 +1,29 @@
 package com.javarush.task.task33.task3310;
 
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+
 /**
  * Created by Taly on 02.08.2018.
  */
 public class Shortener {
-	public Long getId(String string) {
-		return null;
+	private Long lastId = 0L; //будет отвечать за последнее значение идентификатора, которое было использовано при добавлении новой строки в хранилище
+	private StorageStrategy storageStrategy; //будет храниться стратегия хранения данных
+
+	public Shortener(StorageStrategy storageStrategy) { //инициализирует соответствующее поле класса
+		this.storageStrategy = storageStrategy;
 	}
 
-	public String getString(Long id) {
-		return null;
+	public synchronized Long getId(String string) {
+		if (storageStrategy.containsValue(string)) {
+			return storageStrategy.getKey(string);
+		} else {
+			lastId++;
+			storageStrategy.put(lastId, string);
+		}
+		return lastId;
+	}
+
+	public synchronized String getString(Long id) {
+		return storageStrategy.getValue(id);
 	}
 }
